2475201fa23eb0c8985f3beb42320492650dbf20
Angel Duarte <angel.dtd.esc@gmail.com>
2023-01-16 01:37:53 -0600

    order at loading bug resolved

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,124 +1,132 @@
-// console.log("Happy hacking :)");
-
 const url_api = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151";
 const appNode = document.querySelector("#app");
+const allPokemon = [];
 
 async function fetchData() {
-    const allPokemon = [];
     try {
         const response = await fetch(url_api);
-        const info = await response.json();
-        info.results.forEach((item) => {
-            const url_pokemon = item.url;
-            window.fetch(url_pokemon).then((responsePokemon) => responsePokemon.json()).then((infoPokemon) => {
-                const pokemonName = infoPokemon.forms[0].name;
-                // console.log(pokemonName);
-                const name = document.createElement("h2");
-                name.classList.add("pokemon__name");
-                name.textContent = pokemonName;
+        const data = await response.json();
 
-                const pokemonId = infoPokemon.id;
-                const zeros = returnZeros(pokemonId);
-                // console.log(pokemonId);
-                const id = document.createElement("p");
-                id.classList.add("pokemon__id");
-                id.textContent = zeros + pokemonId;
+        data.results.forEach((pokemon) => {
+            const url_pokemon = pokemon.url;
 
-                const pokemonMoves = [];
-                const moves = document.createElement("div");
-                moves.classList.add("pokemon__moves");
-                for (let i = 0; i < 4; i++) {
-                    pokemonMoves.push(infoPokemon.moves[i].move.name);
-                    const move = document.createElement("p");
-                    move.classList.add("pokemon__moves--move");
-                    move.textContent = pokemonMoves[i];
-                    moves.append(move);
-                }
-                // console.log(pokemonMoves);
+            window.fetch(url_pokemon)
+                .then((responsePokemon) => responsePokemon.json())
+                .then((infoPokemon) => {
+                    const pokemonName = infoPokemon.forms[0].name;
+                    // console.log(pokemonName);
 
-                const url_pokemonSprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/${pokemonId}.png`;
-                // console.log(url_pokemonSprite);
-                const img = document.createElement('img');
-                img.classList.add("pokemon__image");
-                img.src = url_pokemonSprite;
+                    const name = document.createElement("h2");
+                    name.classList.add("pokemon__name");
+                    name.innerText = pokemonName;
 
-                const pokemonTypes = [];
-                infoPokemon.types.forEach((type) => {
-                    pokemonTypes.push(type.type.name);
-                });
-                // console.log(pokemonTypes);
-                const types = document.createElement('div');
-                types.classList.add("pokemon__types");
-                for (let i = 0; i < pokemonTypes.length; i++) {
-                    const type = document.createElement('p');
-                    type.classList.add("pokemon__types--type");
-                    switch (pokemonTypes[i]) {
-                        case 'poison':
-                            type.classList.add('pokemon__types--type-poison');
-                            break;
-                        case 'grass':
-                            type.classList.add('pokemon__types--type-grass');
-                            break;
-                        case 'fire':
-                            type.classList.add('pokemon__types--type-fire');
-                            break;
-                        case 'water':
-                            type.classList.add('pokemon__types--type-water');
-                            break;
-                        case 'bug':
-                            type.classList.add('pokemon__types--type-bug');
-                            break;
-                        case 'flying':
-                            type.classList.add('pokemon__types--type-flying');
-                            break;
-                        case 'normal':
-                            type.classList.add('pokemon__types--type-normal');
-                            break;
-                        case 'electric':
-                            type.classList.add('pokemon__types--type-electric');
-                            break;
-                        case 'ground':
-                            type.classList.add('pokemon__types--type-ground');
-                            break;
-                        case 'fairy':
-                            type.classList.add('pokemon__types--type-fairy');
-                            break;
-                        case 'fighting':
-                            type.classList.add('pokemon__types--type-fighting');
-                            break;
-                        case 'psychic':
-                            type.classList.add('pokemon__types--type-psychic');
-                            break;
-                        case 'rock':
-                            type.classList.add('pokemon__types--type-rock');
-                            break;
-                        case 'steel':
-                            type.classList.add('pokemon__types--type-steel');
-                            break;
-                        case 'ice':
-                            type.classList.add('pokemon__types--type-ice');
-                            break;
-                        case 'ghost':
-                            type.classList.add('pokemon__types--type-ghost');
-                            break;
-                        case 'dragon':
-                            type.classList.add('pokemon__types--type-dragon');
-                            break;
+                    const pokemonId = infoPokemon.id;
+                    const zeros = returnZeros(pokemonId);
+                    const id = document.createElement("p");
+                    id.classList.add("pokemon__id");
+                    id.innerText = zeros + pokemonId;
+
+                    // const pokemonMoves = [];
+                    const moves = document.createElement("div");
+                    moves.classList.add("pokemon__moves");
+
+                    for (let i = 0; i < 4; i++) {
+                        try {
+                            // pokemonMoves.push(infoPokemon.moves[i].move.name);
+                            const move = document.createElement("p");
+                            move.classList.add("pokemon__moves--move");
+                            // move.innerText = pokemonMoves[i];
+                            move.innerText = infoPokemon.moves[i].move.name;
+                            moves.append(move);
+                        } catch (error) {
+                            // console.log('error :>> ', error);
+                        }
                     }
-                    type.textContent = pokemonTypes[i];
-                    types.append(type);
-                }
 
-                const pokemonContainer = document.createElement('div');
-                const pokemonContainerP2 = document.createElement('div');
-                pokemonContainerP2.classList.add("pokemon__container--white");
-                pokemonContainerP2.append(types, moves)
-                pokemonContainer.classList.add("pokemon__container");
-                pokemonContainer.append(id, img, name, pokemonContainerP2);
-                allPokemon.push(pokemonContainer);
-                // console.log(allPokemon)
-                appNode.append(...allPokemon);
-            });
+                    const url_pokemonSprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/${pokemonId}.png`;
+                    const img = document.createElement('img');
+                    img.classList.add("pokemon__image");
+                    img.src = url_pokemonSprite;
+
+                    const pokemonTypes = [];
+                    infoPokemon.types.forEach((type) => {
+                        pokemonTypes.push(type.type.name);
+                    });
+                    const types = document.createElement('div');
+                    types.classList.add("pokemon__types");
+                    for (let i = 0; i < pokemonTypes.length; i++) {
+                        const type = document.createElement('p');
+                        type.classList.add("pokemon__types--type");
+                        switch (pokemonTypes[i]) {
+                            case 'poison':
+                                type.classList.add('pokemon__types--type-poison');
+                                break;
+                            case 'grass':
+                                type.classList.add('pokemon__types--type-grass');
+                                break;
+                            case 'fire':
+                                type.classList.add('pokemon__types--type-fire');
+                                break;
+                            case 'water':
+                                type.classList.add('pokemon__types--type-water');
+                                break;
+                            case 'bug':
+                                type.classList.add('pokemon__types--type-bug');
+                                break;
+                            case 'flying':
+                                type.classList.add('pokemon__types--type-flying');
+                                break;
+                            case 'normal':
+                                type.classList.add('pokemon__types--type-normal');
+                                break;
+                            case 'electric':
+                                type.classList.add('pokemon__types--type-electric');
+                                break;
+                            case 'ground':
+                                type.classList.add('pokemon__types--type-ground');
+                                break;
+                            case 'fairy':
+                                type.classList.add('pokemon__types--type-fairy');
+                                break;
+                            case 'fighting':
+                                type.classList.add('pokemon__types--type-fighting');
+                                break;
+                            case 'psychic':
+                                type.classList.add('pokemon__types--type-psychic');
+                                break;
+                            case 'rock':
+                                type.classList.add('pokemon__types--type-rock');
+                                break;
+                            case 'steel':
+                                type.classList.add('pokemon__types--type-steel');
+                                break;
+                            case 'ice':
+                                type.classList.add('pokemon__types--type-ice');
+                                break;
+                            case 'ghost':
+                                type.classList.add('pokemon__types--type-ghost');
+                                break;
+                            case 'dragon':
+                                type.classList.add('pokemon__types--type-dragon');
+                                break;
+                        }
+                        type.innerText = pokemonTypes[i];
+                        types.append(type);
+                    }
+
+                    const pokemonContainer = document.createElement('div');
+
+                    const pokemonContainerP2 = document.createElement('div');
+                    pokemonContainer.classList.add("pokemon__container");
+                    pokemonContainerP2.classList.add("pokemon__container--white");
+
+                    pokemonContainerP2.append(types, moves)
+                    pokemonContainer.append(id, img, name, pokemonContainerP2);
+
+                    allPokemon.push(pokemonContainer);
+                    console.log(allPokemon)
+                    appNode.append(...allPokemon);
+                });
         });
     } catch (error) {
         console.error(error);
